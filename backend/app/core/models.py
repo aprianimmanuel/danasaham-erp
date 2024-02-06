1957f34defb6aa954b7c9fe9cd04ea019dbb1d0c
aprianimmanuel <immanuelaprian@gmail.com>
2024-02-06 16:38:41 +0700

    added user model test && implement user model tests

--- backend/app/core/models.py
+++ backend/app/core/models.py
@@ -1,3 +1,58 @@
-from django.db import models  # noqa
+"""
+Database models.
+"""
 
-# Create your models here.
+from django.db import models
+from django.contrib.auth.models import (
+  AbstractBaseUser,
+  BaseUserManager,
+  PermissionsMixin
+)
+from django.utils.translation import gettext_lazy as _
+
+
+class UserManager(BaseUserManager):
+    """Manager for users."""
+    def create_user(self, email, username, password=None, **extra_fields):
+        if not email:
+            raise ValueError(_('The Email field must be set'))
+        email = self.normalize_email(email)
+        user = self.model(email=email, username=username, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+
+    def create_superuser(self, email, username, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError(_('Superuser must have is_staff=True.'))
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError(_('Superuser must have is_superuser=True.'))
+
+        return self.create_user(email, username, password, **extra_fields)
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    """User in the system."""
+    created_by = models.CharField(max_length=255, blank=True, null=True)
+    created_date = models.DateTimeField(auto_now_add=True)
+    last_updated_date = models.DateTimeField(auto_now=True)
+    email = models.EmailField(_('email_address'), unique=True)
+    username = models.CharField(_('username'), max_length=150, unique=True)
+    is_active = models.BooleanField(_('active'), default=True)
+    is_staff = models.BooleanField(_('staff status'), default=False)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username']
+
+    class Meta:
+        verbose_name = _('user')
+        verbose_name_plural = _('users')
+
+    def __str__(self):
+        return self.email
---
+++ backend/app/core/tests/test_models.py
@@ -0,0 +1,34 @@
+"""Tests for models"""
+
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+
+
+class UserModelTest(TestCase):
+    """Test models."""
+
+    def test_create_user_with_email_successful(self):
+        """Test creating a user with an email is successful."""
+        email = 'test@example.com'
+        password = 'testpass123'
+        user = get_user_model().objects.create_user(
+            email=email,
+            username='testuser',
+            password=password,
+        )
+
+        self.assertEqual(user.email, email)
+        self.assertTrue(user.check_password(password))
+
+    def test_create_superuser(self):
+        """Test creating a superuser"""
+        email = 'admin@example.com'
+        password = 'adminpass123'
+        superuser = get_user_model().objects.create_superuser(
+            email=email,
+            username='adminuser',
+            password=password,
+        )
+
+        self.assertTrue(superuser.is_staff)
+        self.assertTrue(superuser.is_superuser)
