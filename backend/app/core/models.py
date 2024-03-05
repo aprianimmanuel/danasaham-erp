ea1061c05ed168d9c1455044dfb27e558c9228c0
aprianimmanuel <immanuelaprian@gmail.com>
2024-03-05 12:11:57 +0700

    added new model unit test for dttot app

--- backend/app/core/models.py
+++ backend/app/core/models.py
@@ -120,41 +120,42 @@ class UserProfile(models.Model):
 
 
 class dttotDoc(models.Model):
-    created_by = models.ForeignKey(
+    input_by = models.ForeignKey(
         settings.AUTH_USER_MODEL,
         on_delete=models.CASCADE,
         verbose_name=_("Created by User")
     )
-    created_at = models.DateTimeField(
-        _("DTTOT Created at"), auto_now_add=True)
     updated_at = models.DateTimeField(
         _("DTTOT Updated at"), auto_now=True)
     document_id = models.CharField(
         default=uuid.uuid4,
         editable=False,
+        primary_key=True,
         max_length=36
     )
-    dttot_id = models.CharField(
+    dttot_id = models.TextField(
         default=uuid.uuid4,
-        editable=False,
-        max_length=20)
-    dttot_first_name = models.CharField(
+        editable=False
+    )
+    _dttot_first_name = models.TextField(
         _("DTTOT First Name"),
-        max_length=50,
         blank=True)
-    dttot_last_name = models.CharField(
+    _dttot_last_name = models.TextField(
         _("DTTOT Last Name"),
-        max_length=50,
         blank=True)
-    dttot_type = models.CharField(_("DTTOT Type"), max_length=50)
-    dttot_domicile_address1 = models.TextField(
+    dttot_type = models.CharField(
+        _("DTTOT Type"),
+        max_length=50)
+    _dttot_domicile_address1 = models.TextField(
         _("DTTOT Domicile Address"),
         blank=True)
     dttot_domicile_rt = models.IntegerField(
         _("DTTOT Domicile RT"),
+        null=True,
         blank=True)
     dttot_domicile_rw = models.IntegerField(
         _("DTTOT Domicile RW"),
+        null=True,
         blank=True)
     dttot_domicile_kelurahan = models.CharField(
         _("DTTOT Domicile Kelurahan"),
@@ -178,7 +179,226 @@ class dttotDoc(models.Model):
         blank=True)
     dttot_domicile_postal_code = models.IntegerField(
         _("DTTOT Domicile Postal code"),
+        null=True,
         blank=True)
+    dttot_domicile_country = models.CharField(
+        _("DTTOT Domicile Country"),
+        max_length=50,
+        blank=True)
+    _dttot_description_1 = models.TextField(
+        _("DTTOT Description 1"),
+        blank=True)
+    _dttot_description_2 = models.TextField(
+        _("DTTOT Description 2"),
+        blank=True)
+    _dttot_description_3 = models.TextField(
+        _("DTTOT Description 2"),
+        blank=True)
+    dttot_description_4 = models.TextField(
+        _("DTTOT Description 2"),
+        blank=True)
+    dttot_description_5 = models.TextField(
+        _("DTTOT Description 2"),
+        blank=True)
+    _dttot_nik_ktp = models.TextField(
+        _("DTTOT NIK KTP"),
+        blank=True)
+    _dttot_passport_number = models.TextField(
+        _("DTTOT Passport Number"),
+        blank=True)
+    dtott_job = models.TextField(
+        _("DTTOT Job"),
+        blank=True)
+    _dttot_work_number = models.TextField(
+        _("DTTOT Work Number"),
+        blank=True)
+    _dttot_mobile_number = models.TextField(
+        _("DTTOT Mobile Number"),
+        blank=True)
+    _dttot_home_number = models.TextField(
+        _("DTOTT Home Number"),
+        blank=True)
+    _dttot_socialmedia_instagram = models.TextField(
+        _("DTTOT Instagram Accoun"),
+        blank=True)
+    _dttot_socialmedia_facebook = models.TextField(
+        _("DTTOT Facebook Account"),
+        blank=True)
+    _dttot_socialmedia_x = models.TextField(
+        _("DTTOT X account"),
+        blank=True)
+    _dttot_organization_name = models.TextField(
+        _("DTTOT Organization Name"),
+        blank=True)
+    _dttot_kartukeluarga_number = models.TextField(
+        _("DTTOT Nomor Kartu Keluarga"),
+        blank=True)
+    dttot_domicile_city = models.CharField(
+        _("DTTOT Domicile City outside Indonesia"),
+        max_length=50,
+        blank=True)
+    dttot_domicile_district = models.CharField(
+        _("DTTOT Domicile District outside Indonesia"),
+        max_length=50,
+        blank=True)
+    dttot_domicile_subdistrict = models.CharField(
+        _("DTTOT Domicile Subdistrict outside Indonesia"),
+        max_length=50,
+        blank=True)
+    _dttot_kode_densus = models.TextField(
+        _("DTTOT Densus Code"),
+        blank=True)
+    dttot_birth_place_type = models.CharField(
+        _("DTTOT Type of Birth Place that we know (City, Country, etc)"),
+        max_length=50,
+        blank=True)
+    _dttot_birth_place = models.TextField(
+        _("DTTOT Birth Place"),
+        blank=True)
+    _dttot_organization_created_by = models.TextField(
+        _("DTTOT PIC / Founder of Organization"),
+        blank=True)
+
+    # Encrypt sensitive fields before saving
+    def save(self, *args, **kwargs):
+        self._dttot_first_name = fernet.encrypt(
+            self._dttot_first_name.encode()
+        ).decode() if self._dttot_first_name else ''
+        self._dttot_last_name = fernet.encrypt(
+            self._dttot_last_name.encode()
+        ).decode() if self._dttot_last_name else ''
+        self._dttot_domicile_address1 = fernet.encrypt(
+            self._dttot_domicile_address1.encode()
+        ).decode() if self._dttot_domicile_address1 else ''
+        self._dttot_kartukeluarga_number = fernet.encrypt(
+            self._dttot_kartukeluarga_number.encode()
+        ).decode() if self._dttot_kartukeluarga_number else ''
+        self._dttot_kode_densus = fernet.encrypt(
+            self._dttot_kode_densus.encode()
+        ).decode() if self._dttot_kode_densus else ''
+        self._dttot_description_1 = fernet.encrypt(
+            self._dttot_description_1.encode()
+        ).decode() if self._dttot_description_1 else ''
+        self._dttot_description_2 = fernet.encrypt(
+            self._dttot_description_2.encode()
+        ).decode() if self._dttot_description_2 else ''
+        self._dttot_description_3 = fernet.encrypt(
+            self._dttot_description_3.encode()
+        ).decode() if self._dttot_description_3 else ''
+        self._dttot_passport_number = fernet.encrypt(
+            self._dttot_passport_number.encode()
+        ).decode() if self._dttot_passport_number else ''
+        self._dttot_nik_ktp = fernet.encrypt(
+            self._dttot_nik_ktp.encode()
+        ).decode() if self._dttot_nik_ktp else ''
+        self._dttot_work_number = fernet.encrypt(
+            self._dttot_work_number.encode()
+        ).decode() if self._dttot_work_number else ''
+        self._dttot_mobile_number = fernet.encrypt(
+            self._dttot_mobile_number.encode()
+        ).decode() if self._dttot_mobile_number else ''
+        self._dttot_home_number = fernet.encrypt(
+            self._dttot_home_number.encode()
+        ).decode() if self._dttot_home_number else ''
+        self._dttot_socialmedia_instagram = fernet.encrypt(
+            self._dttot_socialmedia_instagram.encode()
+        ).decode() if self._dttot_socialmedia_instagram else ''
+        self._dttot_socialmedia_facebook = fernet.encrypt(
+            self._dttot_socialmedia_facebook.encode()
+        ).decode() if self._dttot_socialmedia_facebook else ''
+        self._dttot_socialmedia_x = fernet.encrypt(
+            self._dttot_socialmedia_x.encode()
+        ).decode() if self._dttot_socialmedia_x else ''
+        self._dttot_organization_name = fernet.encrypt(
+            self._dttot_organization_name.encode()
+        ).decode() if self._dttot_organization_name else ''
+
+        super().save(*args, **kwargs)
+
+    # Decrypt data when accessed
+    @property
+    def dttot_first_name(self):
+        return fernet.decrypt(
+            self._dttot_first_name.encode()
+        ).decode() if self._dttot_first_name else ''
+
+    @property
+    def dttot_last_name(self):
+        return fernet.decrypt(
+            self._dttot_last_name.encode()
+        ).decode() if self._dttot_last_name else ''
+
+    @property
+    def dttot_description_1(self):
+        return fernet.decrypt(
+            self._dttot_description_1.encode()
+        ).decode() if self._dttot_description_1 else ''
+
+    @property
+    def dttot_description_2(self):
+        return fernet.decrypt(
+            self._dttot_description_2.encode()
+        ).decode() if self._dttot_description_2 else ''
+
+    @property
+    def dttot_description_3(self):
+        return fernet.decrypt(
+            self._dttot_description_3.encode()
+        ).decode() if self._dttot_description_3 else ''
+
+    @property
+    def dttot_passport_number(self):
+        return fernet.decrypt(
+            self._dttot_passport_number.encode()
+        ).decode() if self._dttot_passport_number else ''
+
+    @property
+    def dttot_nik_ktp(self):
+        return fernet.decrypt(
+            self._dttot_nik_ktp.encode()
+        ).decode() if self._dttot_nik_ktp else ''
+
+    @property
+    def dttot_work_number(self):
+        return fernet.decrypt(
+            self._dttot_work_number.encode()
+        ).decode() if self._dttot_work_number else ''
+
+    @property
+    def dttot_mobile_number(self):
+        return fernet.decrypt(
+            self._dttot_mobile_number.encode()
+        ).decode() if self._dttot_mobile_number else ''
+
+    @property
+    def dttot_home_number(self):
+        return fernet.decrypt(
+            self._dttot_home_number.encode()
+        ).decode() if self._dttot_home_number else ''
+
+    @property
+    def dttot_socialmedia_instagram(self):
+        return fernet.decrypt(
+            self._dttot_socialmedia_instagram.encode()
+        ).decode() if self._dttot_socialmedia_instagram else ''
+
+    @property
+    def dttot_socialmedia_facebook(self):
+        return fernet.decrypt(
+            self._dttot_socialmedia_facebook.encode()
+        ).decode() if self._dttot_socialmedia_facebook else ''
+
+    @property
+    def dttot_socialmedia_x(self):
+        return fernet.decrypt(
+            self._dttot_socialmedia_x.encode()
+        ).decode() if self._dttot_socialmedia_x else ''
+
+    @property
+    def dttot_organization_name(self):
+        return fernet.decrypt(
+            self._dttot_organization_name.encode()
+        ).decode() if self._dttot_organization_name else ''
 
     def __str__(self):
         return f"{self.dttot_first_name} {self.dttot_last_name} - {self.dttot_type}"  # noqa
--- backend/app/core/tests/test_models.py
+++ backend/app/core/tests/test_models.py
@@ -3,7 +3,9 @@
 from django.test import TestCase
 from django.contrib.auth import get_user_model
 from datetime import date
-from core.models import UserProfile
+from core.models import UserProfile, dttotDoc
+from cryptography.fernet import Fernet
+import os
 
 
 User = get_user_model()
@@ -108,3 +110,79 @@ class UserProfileModelTests(TestCase):
     def test_user_profile_str(self):
         """Test the string representation of the user profile"""
         self.assertEqual(str(self.user_profile), 'testuser')
+
+
+class DttotDocModelTest(TestCase):
+
+    @classmethod
+    def setUp(self):
+        # Setup non-modified objects used by all test methods
+        self.test_user = User.objects.create_user(
+            email='testuser@example.com',
+            username='testuser',
+            password='Testp@ss!23')
+        self.dttot_doc = dttotDoc.objects.create(
+            input_by=self.test_user,
+            _dttot_first_name="John",
+            _dttot_last_name="Doe",
+            dttot_type="Personal",
+            _dttot_domicile_address1="Jalanin aja dulu",
+            _dttot_description_1="KENA GOCEK NIH ABANGKUUWH",
+            _dttot_nik_ktp="1234567890123456",
+            _dttot_passport_number="ABC123456",
+            _dttot_work_number="0123456789",
+            _dttot_mobile_number="0987654321",
+        )
+
+    def test_encryption(self):
+        # Fetch the record directly from the database
+        doc = dttotDoc.objects.get(dttot_id=self.dttot_doc.dttot_id)
+        fernet = Fernet(os.getenv('FERNET_KEY').encode())
+
+        # Check if the sensitive fields are encrypted
+        self.assertNotEqual(doc._dttot_first_name, "John")
+        self.assertTrue(
+            fernet.decrypt(
+                doc._dttot_first_name.encode()
+            ).decode(), "John"
+        )
+
+    def test_decryption(self):
+        # Use the model's properties to check if decryption works correctly
+        self.assertEqual(self.dttot_doc.dttot_first_name, "John")
+        self.assertEqual(self.dttot_doc.dttot_last_name, "Doe")
+        self.assertEqual(
+            self.dttot_doc.dttot_description_1, "KENA GOCEK NIH ABANGKUUWH")
+        self.assertEqual(self.dttot_doc.dttot_nik_ktp, "1234567890123456")
+        self.assertEqual(self.dttot_doc.dttot_passport_number, "ABC123456")
+        self.assertEqual(self.dttot_doc.dttot_work_number, "0123456789")
+        self.assertEqual(self.dttot_doc.dttot_mobile_number, "0987654321")
+
+    def test_str_representation(self):
+        # Test the string representation of the model
+        self.assertEqual(str(self.dttot_doc), "John Doe - Personal")
+
+    def test_update_encrypted_fields(self):
+        # Test updating encrypted fields
+        self.dttot_doc._dttot_first_name = "Jane"
+        self.dttot_doc.save()
+        self.assertEqual(self.dttot_doc.dttot_first_name, "Jane")
+        # Fetch again from DB to ensure changes are persisted
+        updated_doc = dttotDoc.objects.get(dttot_id=self.dttot_doc.dttot_id)
+        self.assertEqual(updated_doc.dttot_first_name, "Jane")
+
+    def test_empty_string_encryption(self):
+        # Test that empty strings in encrypted fields don't raise errors
+        self.dttot_doc._dttot_first_name = ""
+        self.dttot_doc.save()
+        self.assertEqual(self.dttot_doc.dttot_first_name, "")
+        # Ensure empty value is correctly handled
+        empty_val_doc = dttotDoc.objects.get(dttot_id=self.dttot_doc.dttot_id)
+        self.assertEqual(empty_val_doc.dttot_first_name, "")
+
+    def test_user_deletion_cascade(self):
+        # Test dttotDoc instances are deleted when the related User is deleted
+        user_id = self.test_user.user_id
+        self.test_user.delete()
+        with self.assertRaises(dttotDoc.DoesNotExist):
+            dttotDoc.objects.get(input_by=user_id)
